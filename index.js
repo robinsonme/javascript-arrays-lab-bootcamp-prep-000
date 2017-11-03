8188bacac62bba9bd906b57ad9f7c489d4739598
Matthew Robinson <robinson.me@gmail.com>
2017-11-03 00:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(array, name) {
-  array.push(name)
-  return array
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
 }
