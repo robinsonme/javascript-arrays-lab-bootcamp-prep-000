d2d2637b64d947a350e65e59266bc2b5bdd99c34
Matthew Robinson <robinson.me@gmail.com>
2017-11-03 00:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
